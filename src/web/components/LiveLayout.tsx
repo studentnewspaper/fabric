0035ed5d262c13082b60a2a5abc4e4d895f7568d
Nicholas Bush <nickdbush@hotmail.co.uk>
2021-03-11 16:57:14 +0000

    Update live ad

--- src/web/components/LiveLayout.tsx
+++ src/web/components/LiveLayout.tsx
@@ -5,11 +5,11 @@ import ClientGate from "./ClientGate";
 import Advert from "./Advert";
 
 const EMERGENCY_AD_PROPS = {
-  s3MediaId: "xLtnQtqcz5goVkPtVxZ2e",
+  s3MediaId: "KBHuRxiR_98oBINiPNuw8",
   alt: "",
   paidForBy: "EUSA",
   link:
-    "https://www.eusa.ed.ac.uk/yourvoice/elections/march/vote?utm_campaign=GOTV&utm_source=The+Student&utm_medium=Advert",
+    "https://www.eusa.ed.ac.uk/elections?utm_campaign=Results+Night&utm_source=The+Student&utm_medium=Advert",
 };
 
 export interface LiveLayoutProps {
